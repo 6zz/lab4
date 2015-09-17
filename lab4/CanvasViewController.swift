5c3d23759470b33cb2e81c0da6ca5f33b957c420
Shawn Zhu <shawn@tumblr.com>
2015-09-16 21:53:17 -0700

    milestone 2

--- lab4/CanvasViewController.swift
+++ lab4/CanvasViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class CanvasViewController: UIViewController {
 
+    @IBOutlet weak var trayView: UIView!
+    
+    var trayOriginalCenter: CGPoint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,23 @@ class CanvasViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onTrayPanGesture(sender: UIPanGestureRecognizer) {
+        var panGestureRecognizer = sender
+        var point = panGestureRecognizer.locationInView(view)
+        var velocity = panGestureRecognizer.velocityInView(view)
+        
+        if panGestureRecognizer.state == UIGestureRecognizerState.Began {
+            println("Gesture began at: \(point)")
+            trayOriginalCenter = trayView.center
+        } else if panGestureRecognizer.state == UIGestureRecognizerState.Changed {
+            var translation = panGestureRecognizer.translationInView(view)
+            println("Gesture changed at: \(point)")
+            trayView.center = CGPoint(x: trayOriginalCenter.x, y: trayOriginalCenter.y + translation.y)
+        } else if panGestureRecognizer.state == UIGestureRecognizerState.Ended {
+            println("Gesture ended at: \(point)")
+        }
+
+    }
 
 }
 
