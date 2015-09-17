09ad743d1d91b9b6e3e0e4d42604ef8acdf4acf5
Shawn Zhu <shawn@tumblr.com>
2015-09-16 20:26:38 -0700

    add canvas view

---
+++ lab4/CanvasViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  lab4
+//
+//  Created by Shawn Zhu on 9/16/15.
+//  Copyright (c) 2015 Shawn Zhu. All rights reserved.
+//
+
+import UIKit
+
+class CanvasViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
